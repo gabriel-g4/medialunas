a257d692247ac72da43b80690f56e7f96dda4c96
gabriel-g4 <gbgarcia642@gmail.com>
2024-06-07 12:19:53 -0300

    add index and jinja layout

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("layout.html")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
