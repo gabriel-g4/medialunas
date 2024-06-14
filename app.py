2905aeaa22f2eaa76f88a4e788db1acf4380999a
gabriel-g4 <gbgarcia642@gmail.com>
2024-06-13 22:27:31 -0300

    feat: db connection

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
+database = "medialunas.db"
+
+# with sqlite3.connect(database) as connection:
+#     connection.execute("INSERT INTO medialunas (dia, mes, anio, cantidad, tipo)\
+#                         VALUES (?, ?, ?, ?, ?)", (13, 6, 2024, 12, "practica"))
+
+with sqlite3.connect(database) as connection:
+    connection.execute(".schema")
 
 @app.route('/')
 def index():
