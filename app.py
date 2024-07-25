11de06d1a5e9473c7066a138c1926ea14048bc5b
gabriel-g4 <gbgarcia642@gmail.com>
2024-07-25 14:49:10 -0300

    feat: add ingredientes table to /medialunas/id

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ import sqlite3
 
 app = Flask(__name__)
 database = "medialunas.db"
+
 with sqlite3.connect(database) as connection:
     cursor = connection.cursor()
     cursor.execute("SELECT c.name FROM pragma_table_info('medialunas') c")
-    columnas_medialunas = cursor.fetchall()
+    COLUMNAS_MEDIALUNAS = cursor.fetchall()
+    cursor.execute("SELECT c.name FROM pragma_table_info('ingredientes') c")
+    COLUMNAS_INGREDIENTES_PRECIOS = cursor.fetchall()
 
-for i in range(len(columnas_medialunas)):
-    columnas_medialunas[i] = columnas_medialunas[i][0]
+for i in range(len(COLUMNAS_MEDIALUNAS)):
+    COLUMNAS_MEDIALUNAS[i] = COLUMNAS_MEDIALUNAS[i][0]
+    
+for i in range(len(COLUMNAS_INGREDIENTES_PRECIOS)):
+    COLUMNAS_INGREDIENTES_PRECIOS[i] = COLUMNAS_INGREDIENTES_PRECIOS[i][0]
     
 
 
@@ -20,21 +26,38 @@ def index():
         cursor.execute("SELECT * FROM medialunas")
         data = cursor.fetchall()
     return render_template("index.html", medialunas=data\
-                           , columnas_medialunas=columnas_medialunas)
+                           , columnas_medialunas=COLUMNAS_MEDIALUNAS)
 
 @app.route('/medialunas/<int:id>')
 def mostrar_medialunas(id: int):
     with sqlite3.connect(database) as connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM medialunas WHERE id=?", (str(id)))
-        selected_medialuna = cursor.fetchall()
+        cursor.execute("SELECT * FROM medialunas WHERE id=?", (str(id),))
+        selected_medialuna = cursor.fetchone()
+        cursor.execute("SELECT * FROM ingredientes WHERE id=?", (str(id),))
+        selected_ingredientes = cursor.fetchone()
+        cursor.execute("SELECT * FROM precios WHERE id=?", (str(id),))
+        selected_precios = cursor.fetchone()
+    
+    print(COLUMNAS_INGREDIENTES_PRECIOS)
 
     medialuna = {}
-    for i in range(len(columnas_medialunas)):
-        medialuna[columnas_medialunas[i]] = selected_medialuna[0][i]
+    ingredientes = {}
+    precios = {}
+
+    if selected_medialuna != None:
+        for i in range(len(COLUMNAS_MEDIALUNAS)):
+            medialuna[COLUMNAS_MEDIALUNAS[i]] = selected_medialuna[i]
+    if selected_ingredientes != None:
+        for i in range(len(COLUMNAS_INGREDIENTES_PRECIOS)):
+            ingredientes[COLUMNAS_INGREDIENTES_PRECIOS[i]] = selected_ingredientes[i]
+    if selected_precios != None:
+        for i in range(len(COLUMNAS_INGREDIENTES_PRECIOS)):
+            precios[COLUMNAS_INGREDIENTES_PRECIOS[i]] = selected_precios[i]
 
 
-    return render_template("medialunas.html", medialuna=medialuna)
+    return render_template("medialunas.html", medialuna=medialuna, 
+                           ingredientes=ingredientes, precios=precios)
 
 
 if __name__ == "__main__":
