d904866a3b0b9ad99fcf0b93d8c04c5a6daefdea
gabriel-g4 <gbgarcia642@gmail.com>
2024-11-29 12:14:35 -0300

    feat: add index's search bar a general search

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 // index
 
-// Adds a click event to each main table row to go to its
+// Adds a click event to each main table's row to go to its
 // detalle's url.
 
 const tableRows = document.querySelectorAll(".main-row");
@@ -15,11 +15,11 @@ tableRows.forEach(tr => {
 
 // My approach to this problem was to get the main table, and
 // operate on its rows and cells.
-// I used an event on the keyup of the bar, then that value is stored
+// I used an event on the keyup of the bar, then that bar value is stored
 // and later compared to each row value, accesing it by its index
 // The select tag and its options must have the same order
 // as the table columns. (not the same values)
-// If the table in its accessed cell includes the search bar value
+// If the table in its accessed cell includes the search bar's value
 // shows the row, else changes its style.display to none.
 
 const searchBar = document.getElementById("search-bar");
@@ -30,15 +30,43 @@ const length = rows.length;
 searchBar.addEventListener('keyup', (e) => {
   const barValue = searchBar.value.toLowerCase();
   const selectedFilter = document.getElementById("selected-filter");
-  const filterIndex = selectedFilter.selectedIndex;
+  const filterIndex = selectedFilter.selectedIndex - 1 
+  // There is an extra value at the start of the select tag, so I need to 
+  // extract 1 to match the table's columns' length;
   
-  for (let i = 1; i < length; i++) {
-    let col = rows[i].cells;
-    
-    if (!col[filterIndex].innerHTML.toLowerCase().includes(barValue)) {
-      mainTable.rows[i].style.display = "none";
-    } else {
-      mainTable.rows[i].style.display = "table-row";
+
+  if (selectedFilter.value != "-"){
+
+    for (let i = 1; i < length; i++) {
+      
+      let col = rows[i].cells;
+      
+      if (!col[filterIndex].innerHTML.toLowerCase().includes(barValue)) {
+        mainTable.rows[i].style.display = "none";
+      } else {
+        mainTable.rows[i].style.display = "table-row";
+      }
+
+    }
+  } else {
+
+    // In this side of the bifurcation, the selected value is "-"
+    // That means a general search in all rows and columns.
+
+    for (let i = 1; i < length; i++) {
+
+      let col = rows[i].cells;
+      
+      for (let j = 0; j < col.length; j++)  {
+        
+        if (col[j].innerHTML.toLowerCase().includes(barValue)){
+          mainTable.rows[i].style.display = "table-row";
+          break;
+        }
+
+        mainTable.rows[i].style.display = "none";
+
+      } 
     }
   }
 })
